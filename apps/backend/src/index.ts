1d1793bb5239a8d7bc5763aecabd6c9e1b5d4324
/\n!KeT KuMaR <ankitrazz001@gmail.com>
2026-09-21 23:47:03 +0530

    added signIn endpoint

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,12 +2,14 @@ import dotenv from 'dotenv'
 dotenv.config()
 import  express from 'express';
 import {signUp} from "./routes/auth.js"
+import {signIn} from "./routes/auth.js"
 import connectDB from './db/database.js';
 const app = express();
 app.use(express.json());
 
 
 app.post("/signup",signUp)
+app.post("/signin",signIn)
 const PORT = process.env.PORT
 
 connectDB()
--- apps/backend/src/routes/auth.ts
+++ apps/backend/src/routes/auth.ts
@@ -1,6 +1,8 @@
 import type { Request,Response } from "express"
 import users from "../models/user.model.js";
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
+import axios from 'axios'
 
 export const signUp = async(req:Request,res: Response)=>{
   try{
@@ -17,6 +19,47 @@ export const signUp = async(req:Request,res: Response)=>{
     res.status(500).json({
       success: false,
       message: error
+    })
+  }
+}
+
+export const signIn = async(req:Request, res: Response)=>{
+  try{
+  const { email, password } = req.body;
+  if(!email && password){
+    res.status(505).json({
+      messgae: "please enter you email and password"
+    })
+  }
+  const user = await users.findOne({email});
+  if(!user){ 
+    res.status(404).json({
+      message: "could not find the user, please try again"
+    })
+  }
+  const payload = {
+    userId: user?._id.toString(),
+    email : user?.email
+  }
+  const isValid = bcrypt.compare(password, user?.password!)
+      if (!isValid) {
+      return res.status(401).json({
+        error: "invalid credentials",
+      });
+    }
+    const secret = process.env.JWT;
+    if(!secret){
+      return res.send("secret is not given")
+     }
+    const token = jwt.sign(payload,secret, {expiresIn: "7d"}) 
+    return res.status(200).json({
+      user: payload,
+      token,
+      message:"login succefull"
+    });
+  }catch(error){
+    res.status(500).json({
+      message:" login failed"
     })
   }
 }
